32c85121e08d7615d08d7c08f949883d66bedb3a
swana-16 <4323904@students.swinburne.edu.my>
2018-06-21 01:37:39 +0800

    Smart Garden Final Year Project B
    - a firebase based web system completed by sana
    - includes all modifications requested before/after presentation
    - includes major bug fixes
    - improvements to more dynamic, real-time monitoring platform

---
+++ SmartGarden/public/contact/main.js
@@ -0,0 +1,57 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDsh-h-cx_DoC2Q0vyDNZrJC0KwQff6yoU",
+    authDomain: "smartgarden-fyp.firebaseapp.com",
+    databaseURL: "https://smartgarden-fyp.firebaseio.com",
+    projectId: "smartgarden-fyp",
+    storageBucket: "smartgarden-fyp.appspot.com",
+    messagingSenderId: "516399248746"
+  };
+  firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e) {
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('name');
+  var email = getInputVal('email');
+  var phone = getInputVal('phone');
+  var message = getInputVal('message');
+
+  // Save message
+  saveMessage(name, email, phone, message);
+
+  // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id){
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, email, phone, message){
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email: email,
+    phone: phone,
+    message: message
+  });
+}
